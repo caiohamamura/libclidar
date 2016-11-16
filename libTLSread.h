341169fed3b6ad988ceec1885b7d9386b19306c6
StevenHancock <devnull@localhost>
2016-11-15 19:42:18 -0500

    TLS voxelisation routines separated from ALS routines.

--- libTLSread.h
+++ libTLSread.h
@@ -32,8 +32,24 @@
 /*########################################################################*/
 
 
+/*####################################*/
+/*TLS beams, polar coords*/
+
+typedef struct{
+  double zen;     /*zenith*/
+  double az;      /*azimuth*/
+  float x;        /*beam origin*/
+  float y;        /*beam origin*/
+  float z;        /*beam origin*/
+  uint32_t shotN; /*beam number*/  /*is this necessary? Could I save space without it*/
+  uint8_t nHits;  /*number of hits of this beam*/
+  float *r;       /*range*/
+  float *refl;    /*reflectance*/
+}tlsBeam;
+
+
 /*##########################################*/
-/*TLS data*/
+/*TLS point cloud*/
 
 typedef struct{
   int bin;       /*bin number*/
@@ -44,6 +60,46 @@ typedef struct{
   float r;       /*range*/
   uint16_t refl; /*reflectance*/
 }tlsPoint;
+
+
+/*##########################################*/
+/*TLS scan*/
+
+typedef struct{
+  tlsBeam *beams;   /*array of beams*/
+  tlsPoint *points; /*array of points*/
+  double xOff;       /*offset to allow coords to be floats*/
+  double yOff;       /*offset to allow coords to be floats*/
+  double zOff;       /*offset to allow coords to be floats*/
+  uint32_t nBeams;   /*number of beams in this scan*/
+  uint32_t nPoints;  /*number of points in this scan*/
+}tlsScan;
+
+
+/*####################################*/
+/*voxel gap structure*/
+
+typedef struct{
+  /*the voxel*/
+  voxStruct *vox;   /*TLS voxels. Gap within voxel*/
+  voxStruct *toTLS; /*gap to TLS voxels*/
+  float **meanGap;  /*mean minimum gap for voxels*/
+  float **meanRefl; /*mean reflectance for voxels*/
+  int **contN;
+
+  /*TLS map*/
+  int **mapFile;        /*file per voxel*/
+  uint32_t **mapPoint;  /*point per voxel*/
+  int *nIn;             /*number of points per voxel*/
+}tlsVoxStr;
+
+
+/*##########################################*/
+/*function definitions*/
+
+tlsScan *tidyTLScan(tlsScan *);
+tlsScan *readTLSscan(char *,char,char);
+tlsVoxStr *tlsVoxAllocate(int,float *,double *);
 
 /*the end*/
 /*##########################################*/
