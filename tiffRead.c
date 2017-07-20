9486fbb5884a23a6c4c507ebe0908c51536c9af9
StevenHancock <devnull@localhost>
2017-07-20 13:15:07 -0400

    tiffRead.c can now autmocatically read tiled and scanline arranged tiffs, thogh only float data for now. Others could be added later if needed.

--- tiffRead.c
+++ tiffRead.c
@@ -13,10 +13,12 @@
 
 void readGeotiff(geot *geotiff,char *namen,char readData)
 {
-  int i=0;
+  int i=0,j=0,x=0,y=0,place=0;
+  uint32_t tileWidth=0,tileLength=0;
   unsigned int type=0;
   short tiepointsize=0,scalesize=0;
   double *tiepoints=NULL,*scale=NULL;
+  float *buff=NULL;
   //GTIF *tiffStruct=(GTIF*)0;
   TIFF *tiffIn=(TIFF*)0;
 
@@ -35,6 +37,15 @@ void readGeotiff(geot *geotiff,char *namen,char readData)
   TIFFGetField(tiffIn,TIFFTAG_IMAGEWIDTH,&geotiff->nX);
   TIFFGetField(tiffIn,TIFFTAG_IMAGELENGTH,&geotiff->nY);
   TIFFGetField(tiffIn,TIFFTAG_DATATYPE,&type);
+
+
+  /*if the data is tiled*/
+  TIFFGetField(tiffIn,TIFFTAG_TILEWIDTH,&tileWidth);
+  if(tileWidth>0){
+    TIFFGetField(tiffIn,TIFFTAG_TILELENGTH,&tileLength);
+    buff=falloc(tileWidth*tileLength,"buffer",0);
+  }
+
 
   //GTIFPrint(tiffStruct,0,0);
 
@@ -55,10 +66,29 @@ void readGeotiff(geot *geotiff,char *namen,char readData)
     }else if(type==3){ /*float*/
       if(((int)TIFFScanlineSize(tiffIn)/geotiff->nX)==4){
         geotiff->fImage=falloc(geotiff->nX*geotiff->nY,namen,0);
-        for(i=0;i<geotiff->nY;i++){                  /*looping along the lattitude*/
-          if(TIFFReadScanline(tiffIn,&(geotiff->fImage[i*geotiff->nX]),i,1)!=1){
-            fprintf(stderr,"Error reading scan line %d from tiff image\n",i);
-            exit(1);
+
+        if(tileWidth==0){   /*read scan lines*/
+          for(i=0;i<geotiff->nY;i++){                  /*looping along the lattitude*/
+            if(TIFFReadScanline(tiffIn,&(geotiff->fImage[i*geotiff->nX]),i,1)!=1){
+              fprintf(stderr,"Error reading scan line %d from tiff image\n",i);
+              exit(1);
+            }
+          }
+        }else{ /*read tiled data*/
+          for(y=0;y<geotiff->nY;y+=tileLength){                  /*looping along the lattitude*/
+            for(x=0;x<geotiff->nX;x+=tileWidth){
+              TIFFReadTile(tiffIn,buff,x,y,0,0);
+
+              /*pack into results*/
+              for(i=0;i<tileWidth;i++){
+                if((i+x)>=geotiff->nX)continue;
+                for(j=0;j<tileLength;j++){
+                  if((j+y)>=geotiff->nY)continue;
+                  place=x+i+(j+y)*geotiff->nX;
+                  geotiff->fImage[place]=buff[j*tileWidth+i];
+                }
+              }
+            }
           }
         }
       }else if(((int)TIFFScanlineSize(tiffIn)/geotiff->nX)==8){
