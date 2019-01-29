2d431ad5eedd4a8c0c8fec8035ee9f5387450250
StevenHancock <svenhancock@gmail.com>
2019-01-29 19:25:06 +0000

    Comment changed back after a change of heart over splitting the functionality.

--- libLidVoxel.h
+++ libLidVoxel.h
@@ -145,7 +145,7 @@ typedef struct{
 /*TLS scan*/
 
 typedef struct{
-  tlsBeam *beam;     /*array of beams from a riegl*/
+  tlsBeam *beam;     /*array of beams*/
   tlsPoint *point;   /*array of points*/
   double xOff;       /*offset to allow coords to be floats*/
   double yOff;       /*offset to allow coords to be floats*/
